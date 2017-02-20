088861ac10c4b47ddf7d29d95741868238d40e27
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2017-02-20 12:45:10 +0200

    DATE data type test

--- test/testDate.js
+++ test/testDate.js
@@ -6,12 +6,10 @@ var assert       = require('double-check').assert;
 var apersistence = require("../lib/abstractPersistence.js");
 var redis = require("redis");
 var async = require("asynchron");
-
 var callflow = require("callflow");
 
 var redisConnection = async.bindAllMembers(redis.createClient());
 var persistence = apersistence.createRedisPersistence(redisConnection);
-
 
 persistence.registerModel("TestDateModel", {
     name:{
@@ -28,36 +26,29 @@ persistence.registerModel("TestDateModel", {
     assert.equal(err,null, "Error on creating model");
 });
 
-
 var currentDate = new Date();
-
-
 
 assert.callback("Test date", function(end){
     var testSameDate=function(date){
         console.log("Retrieved date:",date,currentDate);
-        assert.equal(currentDate, date,"Dates should be the same.");
+        assert.equal(currentDate.getTime(), date.getTime(),"Dates should be the same.");
         end();
     }
 
     var dateFlowTest = callflow.create("dateTest",{
         begin:function(){
-            //console.log("BEGIN");
             persistence.lookup("TestDateModel", "1d23", this.continue("addDate"));
         },
 
         addDate:function(err, dateObj){
-            //console.log("ADD DATE");
             dateObj['date'] = currentDate;
             persistence.saveObject(dateObj, this.continue("checkDate"));
         },
 
         checkDate:function(err, obj){
-            //console.log("CHECK DATE");
             persistence.lookup("TestDateModel", obj.name, this.continue("validateDate"));
         },
         validateDate:function(err, date){
-            //console.log("VALIDATE DATE");
             testSameDate(date['date']);
         }
 
